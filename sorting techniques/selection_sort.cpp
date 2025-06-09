5413f15b8f09d318c95d20ef63d6eeb78e3b0643
Shubh <shubhyadav@gmail.com>
2025-06-09 21:44:53 +0530

    bubble_sort

--- sorting techniques/selection_sort.cpp
+++ sorting techniques/selection_sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void selection_sort(int arr[], int n){
     for(int i=0; i<n-2; i++){
         int mini=i;
-        for(int j=0; j<n-1;j++){
+        for(int j=i; j<n-1;j++){
             if(arr[j]<arr[mini]){
                 mini=j;
             }
