f1062497fab1ba2c89c98d3e3d66d712b0b6393e
Shubh <shubhyadav085@gmail.com>
2025-08-22 13:56:21 +0530

    Longest subarray with sum K for +ve, negative and zeroes

---
+++ arrays/Medium/longest_subarray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+int longestSubarrayWithSumK(vector<int> a, long long k){
+    map<long, long> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+
+    for(int i=0; i<a.size(); i++){
+        sum += a[i];
+        if(sum == k){
+            maxLen= max(maxLen, i+1);
+        }
+        long long rem = sum - k;
+        if(preSumMap.find(rem)!= preSumMap.end()){
+            int len= i- preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        if(preSumMap.find(sum) == preSumMap.end()){
+            preSumMap[sum] = i;
+        }
+    }
+    return maxLen;
+
+}
+
+// This is the optimal solution for negatives , positives and zeroes in the array.
