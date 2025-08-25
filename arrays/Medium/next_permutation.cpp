79091b2a7831aafd3f8cc2e43785f70d746b2d0b
Shubh <shubhyadav085@gmail.com>
2025-08-25 20:48:28 +0530

    Next permutation array

---
+++ arrays/Medium/next_permutation.cpp
@@ -0,0 +1,26 @@
+void nextPermutation(vector<int> &a){
+    int ind=-1;
+    int n=a.size();
+
+    for(int i=n-2; i>=0; i--){
+        if(a[i]<a[i+1]){
+            ind=i;
+            break;
+        }
+    }
+    if(ind==-1){
+        reverse(a.begin(), a.end());
+        return
+    }
+
+    for(int i=n-1; i>ind; i++){
+        if(nums[i]>nums[ind]){
+            swap(nums[i], nums[ind]);
+            return;
+        }
+    }
+    reverse(nums.begin() + ind+1, nums.end());
+}
+
+// TC: O(3N)
+// SC: O(1)
