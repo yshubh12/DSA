7c6f60cbc23cbb0c01e5f4d7c58ec1049a78fbaa
Shubh <shubhyadav085@gmail.com>
2025-08-25 18:11:12 +0530

    Stock Buy and Sell

---
+++ arrays/Medium/stock_buy_sell.cpp
@@ -0,0 +1,11 @@
+int maxProfit(vector<int> prices){
+    int profit=0;
+    int mini=prices[0];
+
+    for(int i=1; i<prices.size(); i++){
+        int cost=prices[i]-mini;
+        profit=max(cost, profit);
+        mini=min(a[i], mini);
+    }
+    return profit;
+}
