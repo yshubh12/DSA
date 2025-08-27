caa7ccae255f65a359dec577964279d28dee15fa
Shubh <shubhyadav085@gmail.com>
2025-08-27 18:57:17 +0530

    Pascal's Triangle

---
+++ arrays/Hard/Pascal_triangle.cpp
@@ -0,0 +1,19 @@
+int generateRow(int row){
+    long long ans=1;
+    vector<int> ansRow;
+    ansRow.push_back(1);
+    for(int col=1; col<row; col++){
+        ans=ans*(row-col);
+        ans=ans/col;
+        ansRow.push_back(ans);
+    }
+    return ans;
+}
+
+vector<vector<int>> generate(int N){
+    vector<vector<int>> ans;
+    for(int i=1; i<=N; i++){
+        ans.push_back(generateRow(i));
+    }
+    return ans;
+}
