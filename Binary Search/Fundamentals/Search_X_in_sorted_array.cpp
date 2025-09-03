d0b64ec13b74e1158b5a6e562614cafa5dc93197
Shubh <shubhyadav085@gmail.com>
2025-09-03 19:36:41 +0530

    Search X in a sorted array

--- Binary Search/Fundamentals/Search_X_in_sorted_array.cpp
+++ Binary Search/Fundamentals/Search_X_in_sorted_array.cpp
@@ -15,3 +15,29 @@ int search(vector<int>& nums, int target) {
 
 // TC: O(log n) -- binary search
 // SC: O(1)
+
+
+
+// If array have multiple target elements return the smallest index
+
+class Solution {
+  public:
+    int binarysearch(vector<int> &arr, int k) {
+        int n=arr.size();
+        int low=0;
+        int high=n-1;
+        int ans=-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(arr[mid]<k) low=mid+1;
+            else if(arr[mid]>k){
+                high=mid-1;
+            }
+            else{
+                ans=mid;
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
