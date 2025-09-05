5b2e17a135fe4655964d2a19397cd76df2a3fd2f
Shubh <shubhyadav085@gmail.com>
2025-09-05 21:14:42 +0530

    Find Nth root of a number

---
+++ Binary Search/On Answers/Nth_root.cpp
@@ -0,0 +1,23 @@
+// 'n' is the root number
+// 'm' is the number
+
+int func(int mid, int n, int m){
+    long long ans=1;
+    for(int i=1; i<=n; i++){
+        ans=ans*mid;
+        if(ans>mid) return 2;
+    }
+    if(ans==mid) return 1;
+    return 0;
+}
+
+int nthRoot(int n,int m){
+    int low=1, high=m;
+    while(low<=high){
+        int mid=(low+high)/2;
+        int midF=func(mid,n,m);
+        if(midF==1) return mid;
+        else if(midF==2) return high=mid-1;
+        else low=mid+1;
+    }
+}
