6849d95d3b309aabbdb1c72ec3fd09813be7c391
Shubh <shubhyadav085@gmail.com>
2025-09-10 15:35:09 +0530

    smallest capacity to ship all packages with M dayss

---
+++ Binary Search/On Answers/capacity_to_ship_pacakages.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int daysReq(vector<int>& weights, int capacity){
+        int days=1, load=0;
+        for(int i=0; i<weights.size(); i++){
+            if(load+weights[i]>capacity){
+                days+=1;
+                load=weights[i];
+            }
+            else load+=weights[i];
+        }
+        return days;
+    }
+    int maxCapacity(vector<int>& weights){
+        int sum=0;
+        for(int i=0; i<weights.size(); i++){
+            sum+=weights[i];
+        }
+        return sum;
+    }
+    int maxEle(vector<int>& weights){
+        int maxi=INT_MIN;
+        for(int i=0; i<weights.size(); i++){
+            maxi=max(maxi,weights[i]);
+        }
+        return maxi;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low=maxEle(weights), high=maxCapacity(weights);
+        while(low<=high){
+            int mid=(low+high)/2;
+            int usedDays=daysReq(weights, mid);
+            if(usedDays<=days) high=mid-1;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
