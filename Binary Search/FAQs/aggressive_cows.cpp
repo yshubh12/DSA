d87b8fd7bfde8fbf0031c4f9770826cd9b984541
Shubh <shubhyadav085@gmail.com>
2025-09-14 12:19:20 +0530

    maximum of minimum distance between two cows

---
+++ Binary Search/FAQs/aggressive_cows.cpp
@@ -0,0 +1,25 @@
+bool canWePlace(vector<int>& stalls, int dist, int cows){
+    int cntCows=1, last=stalls[0];
+    for(int i=1; i<stalls.size(); i++){
+        if(stalls[i]-last>=dist){
+            last=stalls[i];
+            cntCows++;
+        }
+        if(cntCows>=cows) return true;
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int>& stalls, int k){
+    sort(stalls.begin(), stalls.end());
+    int n=stalls.size();
+    int low=1, high=stalls[n-1]- stalls[0];
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(canWePlace(stalls, mid, k)==true) low=mid+1;
+        else high=mid-1;
+    }
+    return high;
+}
+
+// TC:O(n log n)
